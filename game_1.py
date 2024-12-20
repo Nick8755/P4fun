6d92cad9bb3b05e5030602b9674587c043d01815
Nick8755 <rocl.na85@gmail.com>
2024-12-20 03:01:56 +0100

    add balance, bet, number of lines

--- game_1.py
+++ game_1.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 1000
+MIN_BET = 1
+
 def deposit ():
     while True:
         amount = int(input("Enter the amount to deposit: "))
@@ -7,4 +11,31 @@ def deposit ():
             print("Invalid amount. Amount must be greater than 0. Please try again.")
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = int(input("Enter the number of lines: "))
+        if lines > 0 and lines <= MAX_LINES:
+            break
+        else:
+            print(f"Invalid number of lines. Number of lines must be between 1 and {MAX_LINES}. Please try again.")
+    return lines
+
+def get_bet_amount(balance):
+    while True:
+        bet = int(input("Enter the bet amount: "))
+        if bet > balance:
+            print(f"Insufficient balance. Bet amount must be less than or equal to your balance of {balance}. Please try again.")
+        elif bet >= MIN_BET and bet <= MAX_BET:
+            break
+        else:
+            print(f"Invalid bet amount. Bet amount must be between {MIN_BET} and {MAX_BET}. Please try again.")
+    return bet
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(f"Your balance is: {balance} and the number of lines is: {lines}")
+    print("Your bet amount is: ", get_bet_amount(balance))
+
+main()
+
